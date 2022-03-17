5a5855286eb5d16d6bdd3c5e20fd63835f85d6c7
Johnson <ktjohnson@loaner5033.fullsail.com>
2022-03-17 16:52:37 -0400

    added files, put the wrong theme in and it erased everthing. Reverted to an old commit

--- form-refresher/src/components/UI/ErrorModal.js
+++ form-refresher/src/components/UI/ErrorModal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "./Card";
-import Button from "./Button";
+import Button from "./button";
 import classes from "./ErrorModal.module.css";
 import ReactDOM from "react-dom";
 
